c8f81e1375e633a8eeb0d25589d100debba6b31a
Robert Vale <robertvale301104@gmail.com>
2020-11-28 22:41:48 -0500

    Modified coomand name

--- cmd/watch.go
+++ cmd/watch.go
@@ -3,16 +3,15 @@ package cmd
 
 import (
 	"fmt"
-	fs "github.com/fsnotify/fsnotify"
 	"github.com/spf13/cobra"
-	"log"
-	"os/exec"
 )
 
 // buildCmd represents the build command
 var buildCmd = &cobra.Command{
-	Use:   "build",
-	Short: "Start the auto-compiling process",
+	Use:     "watch [-R] file",
+	Aliases: []string{"w"},
+	Short:   "Start the auto-compiling process",
+	Example: "watch -R main.go",
 	Run: func(cmd *cobra.Command, filename []string) {
 		// get the first argument
 		f := filename[0]
@@ -21,61 +20,16 @@ var buildCmd = &cobra.Command{
 		// Establish a watcher for the file we want to observe for changes
 		watch(f)
 	},
+	PostRun: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Ended running")
+	},
 }
 
 // all the logic behind changing file monitoring
 func watch(filename string) {
-	watcher, err := fs.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
-
-	done := make(chan bool)
-	go func() {
-		for {
-			select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					//return
-				}
-				if event.Op&fs.Write == fs.Write {
-					func() {
-						fmt.Println("build initialized...")
-
-						// specify the file we want to look at and the command
-						cmd := exec.Command("go", "build", filename)
-
-						// start compiling the file calling 'go build' and look for errors
-						stdoutStderr, err := cmd.CombinedOutput()
-						if err != nil {
-							log.Fatal(err)
-						}
-						fmt.Printf("%s\n", stdoutStderr)
-
-						fmt.Printf("File %s built succesfully\n", filename)
-
-					}()
-				}
-			// return the function if not ok or log the error whether there's any
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("error:", err)
-			}
-		}
-	}()
-
-	// start watching the file specified
-	err = watcher.Add(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
-	<-done
+	fmt.Printf("watching: %s\n", filename)
 }
 
 func init() {
 	rootCmd.AddCommand(buildCmd)
 }
-
